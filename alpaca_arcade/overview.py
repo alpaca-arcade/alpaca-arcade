dbb2bc04276f318c954ba0c982e0648096e37f68
Josh Garber <josh@jkgarber.com>
2025-08-22 15:52:50 +0200

    make overview and play routes dynamic and start breakout pages

--- alpaca_arcade/overview.py
+++ alpaca_arcade/overview.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 from .scores import get_scores, creds_found
 
 
@@ -7,10 +7,20 @@ bp = Blueprint("overview", __name__)
 
 @bp.route("/")
 def index():
-    context = {
-    'minesweeper_scores_easy': get_scores("minesweeper", 0),
-    'minesweeper_scores_medium': get_scores("minesweeper", 1),
-    'minesweeper_scores_hard': get_scores("minesweeper", 2),
-    'hcaptcha_credentials_found': creds_found
-    }
-    return render_template("overview/index.html", **context)
+    game = request.args.get("game", "")
+    error = None
+    if game not in ["minesweeper", "breakout"]:
+        error = "invalid or missing game"
+    if error:
+        return error, 400
+
+    context = {}
+    if game == "minesweeper":
+        context = {
+        'minesweeper_scores_easy': get_scores("minesweeper", 0),
+        'minesweeper_scores_medium': get_scores("minesweeper", 1),
+        'minesweeper_scores_hard': get_scores("minesweeper", 2),
+        'hcaptcha_credentials_found': creds_found
+        }
+
+    return render_template(f"overview/{game}.html", **context)
--- alpaca_arcade/play.py
+++ alpaca_arcade/play.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 from .scores import get_credential, creds_found
 
 bp = Blueprint("play", __name__)
 
 @bp.route("/")
 def index():
+    game = request.args.get("game", "")
+    error = None
+    if game not in ["minesweeper", "breakout"]:
+        error = "invalid or missing game"
+    if error:
+        return error, 400
+
     hcaptcha_sitekey = get_credential("HCAPTCHA_SITEKEY")
     context = {
         'hcaptcha_sitekey': hcaptcha_sitekey,
         'hcaptcha_credentials_found': creds_found
     }
-    return render_template("play/index.html", **context)
+
+    return render_template(f"play/{game}.html", **context)
