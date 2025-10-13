6e5318e76aa8b5fd05cd8da5468f8d8340db2a65
Josh Garber <josh@jkgarber.com>
2025-10-13 17:26:57 +0200

    added wall bounds and enlarged the canvas

--- alpaca_arcade/static/breakout.js
+++ alpaca_arcade/static/breakout.js
@@ -7,14 +7,15 @@ class ExampleScene extends Phaser.Scene {
         this.ball = this.add.sprite(50, 50, "ball");
         this.physics.add.existing(this.ball);
         this.ball.body.setVelocity(150, 150);
+        this.ball.body.setCollideWorldBounds(true, 1, 1);
     }
     update() {}
 }
 
 const config = {
     type: Phaser.CANVAS,
-    width: 480,
-    height: 320,
+    width: 800,
+    height: 600,
     scene: ExampleScene,
 	parent: document.querySelector(".breakout-container"),
     scale: {
