9a093ccfc3e7df5282afa3c2911fdd3fbd9e3ad4
Josh Garber <josh@jkgarber.com>
2025-10-16 21:20:27 +0200

    added paddle and collision

--- alpaca_arcade/static/breakout.js
+++ alpaca_arcade/static/breakout.js
@@ -1,15 +1,33 @@
 class ExampleScene extends Phaser.Scene {
     ball;
+    paddle;
     preload() {
         this.load.image("ball", "/static/images/ball.png");
+        this.load.image("paddle", "/static/images/paddle.png");
     }
     create() {
-        this.ball = this.add.sprite(50, 50, "ball");
+        this.ball = this.add.sprite(
+            this.scale.width * 0.5,
+            this.scale.height - 25,
+            "ball",
+        );
         this.physics.add.existing(this.ball);
-        this.ball.body.setVelocity(150, 150);
+        this.ball.body.setVelocity(150, -150);
         this.ball.body.setCollideWorldBounds(true, 1, 1);
+        this.ball.body.setBounce(1);
+        this.paddle = this.add.sprite(
+            this.scale.width * 0.5,
+            this.scale.height - 5,
+            "paddle",
+        );
+        this.paddle.setOrigin(0.5, 1);
+        this.physics.add.existing(this.paddle);
+        this.paddle.body.setImmovable(true);
     }
-    update() {}
+    update() {
+        this.physics.collide(this.ball, this.paddle);
+        this.paddle.x = this.input.x || this.scale.width * 0.5;
+    }
 }
 
 const config = {
@@ -22,7 +40,7 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    backgroundColor: "#eeeeee",
+    backgroundColor: 0x000157,
     physics: {
         default: "arcade",
     },
